ca3bec4f2b122d97292d0b7f5ba01d5754a7b637
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-13 05:28:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,4 +6,16 @@ class School
     @roster = { }
   end
 
+def add_student(name, grade)
+    @roster[grade] = [ ]
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[:grade]
+  end
+  
+  def sort 
+    @roster.sort
+  end
 end
